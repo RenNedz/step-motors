0c0b5e0dd6e6f9180d583b6b18505e91892c4f77
Renatas Nedzveckas <renatas.nedzveckas2@mail.dcu.ie>
2019-04-28 13:18:44 +0100

    Added upload image for listing main image. Working now

--- create-listing.php
+++ create-listing.php
@@ -27,31 +27,45 @@ function moveImagesToListingDirectory($imageDirectoryPath) {
     if(isset($_FILES['files'])){
         $file_ary = reArrayFiles($_FILES['files']);
         $file_id = 1;
-        debug_to_console($imageDirectoryPath);
         foreach ($file_ary as $file) {
             $image_filename = $file_id . $file['name'];
-            debug_to_console('filename: ' . $image_filename);
             $file_id++;
             $file_tmp =$file['tmp_name'];
             $successful = move_uploaded_file($file_tmp, $imageDirectoryPath . $image_filename);
-            if ($successful) {
-                debug_to_console('file move success: ');
-            } else {
-                debug_to_console('file move failure ');
-            }
         }
     }
 }
 
+function moveListingMainImageToListingImages($listingNumber) {
+    $listingImageTargetDir = "listing-images/";
+    if(isset($_FILES['listingImage'])){
+        $file = $_FILES['listingImage'];
+        debug_to_console($listingImageTargetDir);
+        $image_filename = $listingNumber . $file['name'];
+        debug_to_console('filename: ' . $image_filename);
+        $file_tmp = $file['tmp_name'];
+        $successful = move_uploaded_file($file_tmp, $listingImageTargetDir . $image_filename);
+        if ($successful) {
+            return $listingImageTargetDir . $image_filename;
+        } else {
+            return "";
+        }
+    }
+}
 
 // Only call this function ONCE
-function getListingBaseName($fields){
+function getListingNumber() {
     $count = trim(file_get_contents('listing-generator-files/listingcount.txt'));
     $count++;
     $counterFile = fopen('listing-generator-files/listingcount.txt', "w");
     fwrite($counterFile, $count);
     fclose($counterFile);
-    return str_replace(' ', '-', strtolower($fields["listingName"])) . $count;
+    return $count;
+}
+
+
+function getListingBaseName($fields, $listingNumber){
+    return str_replace(' ', '-', strtolower($fields["listingName"])) . $listingNumber;
 }
 
 function getListingHtml($fields) {
@@ -68,7 +82,7 @@ function getListingFilename($fields, $listingBaseName) {
     return 'listings/' . $listingBaseName . '/' . $listingBaseName . '.html';
 }
 
-function addListingToListingsPage($listingFileName, $fields){
+function addListingToListingsPage($listingFileName, $fields, $listingImageFilePath){
     $nextListingText = '<div style="visibility: hidden"><p>$nextListing$</p></div>';
 
     $allListingsHtml = trim(file_get_contents('vehicle-sale-listings.html'));
@@ -76,7 +90,7 @@ function addListingToListingsPage($listingFileName, $fields){
     $vehicleSaleListing = str_replace('$LISTING-NAME$', $fields["listingName"], $vehicleSaleListing);
     $vehicleSaleListing = str_replace('$LISTING-DESCRIPTION$', $fields["listingDescription"], $vehicleSaleListing);
     $vehicleSaleListing = str_replace('$LISTING-LINK$', $listingFileName, $vehicleSaleListing);
-    $vehicleSaleListing = str_replace('$IMG-SRC$', 'images/dad.jpg', $vehicleSaleListing);
+    $vehicleSaleListing = str_replace('$IMG-SRC$', $listingImageFilePath, $vehicleSaleListing);
     $vehicleSaleListing = $vehicleSaleListing . '
     '. $nextListingText;
     $allListingsHtml = str_replace($nextListingText, $vehicleSaleListing, $allListingsHtml);
@@ -102,15 +116,16 @@ try
     foreach ($_POST as $key => $value) {
         $fields += array($key => $value);
     }
-    $listingBaseName = getListingBaseName($fields);
+    $listingNumber = getListingNumber();
+    $listingBaseName = getListingBaseName($fields, $listingNumber);
     $listFilename = getListingFilename($fields, $listingBaseName);
     $listingFile = fopen($listFilename, "w");
     $listingHTML = getListingHtml($fields);
     fwrite($listingFile, $listingHTML);
     fclose($listingFile);
-    addListingToListingsPage($listFilename, $fields);
-
+    $listingImageFilePath = moveListingMainImageToListingImages($listingNumber);
     moveImagesToListingDirectory("listings/" . $listingBaseName . '/');
+    addListingToListingsPage($listFilename, $fields, $listingImageFilePath);
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
