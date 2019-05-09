8d6d21ecd0ce15eed14d79eb83714c06801d78ae
Renatas Nedzveckas <renatas.nedzveckas2@mail.dcu.ie>
2019-05-09 21:28:34 +0100

    listing html structure updated. New fields added

--- create-listing.php
+++ create-listing.php
@@ -24,6 +24,7 @@ function reArrayFiles(&$file_post) {
 }
 
 function moveImagesToListingDirectory($imageDirectoryPath) {
+    $imageFilenameArray = [];
     if(isset($_FILES['files'])){
         $file_ary = reArrayFiles($_FILES['files']);
         $file_id = 1;
@@ -31,9 +32,11 @@ function moveImagesToListingDirectory($imageDirectoryPath) {
             $image_filename = $file_id . $file['name'];
             $file_id++;
             $file_tmp =$file['tmp_name'];
-            $successful = move_uploaded_file($file_tmp, $imageDirectoryPath . $image_filename);
+            move_uploaded_file($file_tmp, $imageDirectoryPath . $image_filename);
+            array_push($imageFilenameArray, $image_filename);
         }
     }
+    return $imageFilenameArray;
 }
 
 function moveListingMainImageToListingImages($listingNumber) {
@@ -68,14 +71,34 @@ function getListingBaseName($fields, $listingNumber){
     return str_replace(' ', '-', strtolower($fields["listingName"])) . $listingNumber;
 }
 
-function getListingHtml($fields) {
+function getListingHtml($fields, $imageFilenameArray) {
     $templateHtml = trim(file_get_contents('listing-generator-files/listing-template.html'));
+    if (count($imageFilenameArray) > 0) {
+        $carouselSlideHtml = '<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>';
+        $inactiveSlideHtml = '<li data-target="#carouselExampleIndicators" data-slide-to="COUNT"></li>';
+        $count = 1;
+        while ($count != count($imageFilenameArray)){
+            $carouselSlideHtml = $carouselSlideHtml . str_replace('COUNT', $count, $inactiveSlideHtml);
+            $count++;
+        }
+        $carouselImagesHtml = '';
+        $imageHtml = '<div class="carousel-item"><img class="d-block w-100" src="$IMAGE$" alt="This is an image of the vehicle up for sale."></div>';
+        foreach ($imageFilenameArray as &$value) {
+            $carouselImagesHtml = $carouselImagesHtml .  str_replace('$IMAGE$', $value, $imageHtml);
+        }
+        $carouselImagesHtml = preg_replace('/' . 'carousel-item' . '/', 'carousel-item active', $carouselImagesHtml, 1);
+        $templateHtml = str_replace('$CAROUSEL-SLIDES$', $carouselSlideHtml, $templateHtml);
+        $templateHtml = str_replace('$LISTING-IMAGES$', $carouselImagesHtml, $templateHtml);
+    }
     $templateHtml = str_replace('$LISTING-NAME$', $fields["listingName"], $templateHtml);
     $templateHtml = str_replace('$LISTING-BODY$', $fields["listingBody"], $templateHtml);
-
-
+    $templateHtml = str_replace('<p style="visibility: hidden; padding: 10px;"><span class="stepMotorRedBlackOps">Doors:</span> $NUMBER-OF-DOORS$</p>', '<p style="padding: 10px;"><span class="stepMotorRedBlackOps">Doors:</span> ' . $fields["numberDoors"] . '</p>', $templateHtml);
+    $templateHtml = str_replace('$ENGINE$', $fields["engineSize"], $templateHtml);
+    $templateHtml = str_replace('$REGYEAR$', $fields["registrationYear"], $templateHtml);
+    $templateHtml = str_replace('$VEHICLE-COLOUR$', $fields["vehicleColour"], $templateHtml);
+    $templateHtml = str_replace('$MILEAGE$', $fields["vehicleMileage"], $templateHtml);
     return $templateHtml;
-}
+} //TO DO: ------------------- ADD IF STATEMENT FOR NON MANDATORY FIELDS LIKE DOORS
 
 function getListingFilename($fields, $listingBaseName) {
     mkdir("listings/" . $listingBaseName, 0777);
@@ -119,12 +142,12 @@ try
     $listingNumber = getListingNumber();
     $listingBaseName = getListingBaseName($fields, $listingNumber);
     $listFilename = getListingFilename($fields, $listingBaseName);
+    $imageFilenameArray = moveImagesToListingDirectory("listings/" . $listingBaseName . '/');
+    $listingHTML = getListingHtml($fields, $imageFilenameArray);
     $listingFile = fopen($listFilename, "w");
-    $listingHTML = getListingHtml($fields);
     fwrite($listingFile, $listingHTML);
     fclose($listingFile);
     $listingImageFilePath = moveListingMainImageToListingImages($listingNumber);
-    moveImagesToListingDirectory("listings/" . $listingBaseName . '/');
     addListingToListingsPage($listFilename, $fields, $listingImageFilePath);
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
